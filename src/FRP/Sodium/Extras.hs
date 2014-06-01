52d34bf171de9a1b974bea58699202bb039ff456
Dmitry Ivanov <ethercrow@gmail.com>
2014-06-02 01:34:06 +0700

    Generalize uniqueUpdates.

--- src/FRP/Sodium/Extras.hs
+++ src/FRP/Sodium/Extras.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE MonadComprehensions #-}
 module FRP.Sodium.Extras
     ( uniqueUpdates
+    , updatesSatisfyingRelation
     ) where
 
-import qualified FRP.Sodium as Sodium
+import qualified FRP.Sodium as S
 
--- | Like 'FRP.Sodium.values' but consecutive values are guaranteed to be non-equal
-uniqueUpdates :: Eq a => Sodium.Behavior a -> Sodium.Reactive (Sodium.Event a)
-uniqueUpdates behavior = do
-    initial <- Sodium.sample behavior
-    let possiblyDuplicateValues = Sodium.updates behavior
-    uniqueValues <- Sodium.collectE (\new old -> ([new | new /= old], new))
+-- | Like 'FRP.S.values' but consecutive values are guaranteed to be non-equal.
+uniqueUpdates :: Eq a => S.Behavior a -> S.Reactive (S.Event a)
+uniqueUpdates = updatesSatisfyingRelation (/=)
+
+-- | Like 'FRP.S.values' but consecutive values are guaranteed to
+--   satisfy the passed relation.
+updatesSatisfyingRelation :: (a -> a -> Bool) -> S.Behavior a -> S.Reactive (S.Event a)
+updatesSatisfyingRelation rel behavior = do
+    initial <- S.sample behavior
+    let possiblyDuplicateValues = S.updates behavior
+    uniqueValues <- S.collectE (\new old -> ([new | rel old new], new))
                                     initial
                                     possiblyDuplicateValues
-    return $! Sodium.filterJust uniqueValues
+    return $! S.filterJust uniqueValues
